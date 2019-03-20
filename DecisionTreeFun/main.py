097ac5a86197948286157c67d6590e99a3afc5df
Gina Sprint <sprint@gonzaga.edu>
2019-03-20 13:48:28 -0700

    first half of tree

--- DecisionTreeFun/main.py
+++ DecisionTreeFun/main.py
@@ -18,6 +18,28 @@ def main():
             ["Mid", "Java", "yes", "no", "True"],
             ["Junior", "Python", "no", "yes", "False"]
         ]
+    # how to represent trees in Python?
+    #1. OOP
+    #2. nested data structures like lists!
+    # we will use nested lists
+    # task: Finish the tree!
+    interview_tree = \
+    ["Attribute", "level",
+        ["Value", "Senior",
+            ["Attribute", "tweets",
+                ["Value", "yes",
+                    ["Leaves", ["True", 2, 5, 40]]
+                ],
+                ["Value", "no",
+                    ["Leaves", ["False", 3, 5, 60]]
+                ]
+            ]
+        ],
+        ["Value", "Mid"
+        ],
+        ["Value", "Junior"
+        ]
+    ]
 
 if __name__ == "__main__":
     main()
