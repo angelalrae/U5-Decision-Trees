a4c6375d635bf8565db3335dbd172c20f100ccdd
Gina Sprint <sprint@gonzaga.edu>
2019-03-21 08:22:11 -0700

    partition

--- DecisionTreeFun/main.py
+++ DecisionTreeFun/main.py
@@ -50,6 +50,16 @@ def main():
         ]
     ]
 
+    # dictionary of attribute index: attribute value pairs
+    att_domains = {0: ["Senior", "Mid", "Junior"], 
+        1: ["R", "Python", "Java"],
+        2: ["yes", "no"], 
+        3:["yes", "no"]}
+    # att_indexes is a list of attributes to use for building the tree
+    att_indexes = list(range(len(header) - 1))
+    class_index = len(header) - 1
+    tree = tdidt(table, att_indexes, att_domains, class_index, header)
+
 def tdidt(instances, att_indexes, att_domains, class_index, header=None):
     # Basic Approach (uses recursion!):
 
@@ -58,13 +68,29 @@ def tdidt(instances, att_indexes, att_domains, class_index, header=None):
     # can't choose the same attribute twice in a branch!!
     att_indexes.remove(att_index) # remember: Python is pass
     # by object reference!! 
+    print("Splitting on:", att_index, header[att_index])
 
     # Partition data by attribute values ... this creates pairwise disjoint partitions
     partition = partition_instances(instances, att_index, att_domains[att_index])
+    print(partition)
     # Repeat until one of the following occurs (base cases):
     # Partition has only class labels that are the same ... no clashes, make a leaf node
     # No more attributes to partition ... reached the end of a branch and there may be clashes, see options below
     # No more instances to partition ... see options below
+
+    return None
+
+def partition_instances(instances, att_index, att_domain):
+    # this is a group by att_domain, not by att_values in instances
+    partition = {}
+    for att_value in att_domain:
+        subinstances = []
+        for instance in instances:
+            # check if this instance has att_value at att_index
+            if instance[att_index] == att_value:
+                subinstances.append(instance)
+        partition[att_value] = subinstances
+    return partition
 
 def select_attribute(instances, att_indexes, class_index):
     # random selection for now
